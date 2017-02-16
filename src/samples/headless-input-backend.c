b8c56efe7fce0bab9d06ef3f240b6bca17fa337e
JengHyun Kang <jhyuni.kang@samsung.com>
2017-02-16 22:06:45 +0900

    headless: Support multiple input devices in input backend
    Change-Id: I9676556726883fef798d4a3ff26f67778a4bcfe9

--- src/samples/headless-input-backend.c
+++ src/samples/headless-input-backend.c
@@ -37,29 +37,39 @@ PEPPER_API int headless_input_backend_fini(void *);
 
 static const char *_headless_input_device_get_property(void *device, const char *key);
 
+typedef struct input_backend_data input_backend_data_t;
+typedef struct device_data device_data_t;
 typedef struct key_event key_event_t;
 
-typedef struct _Input_Global_DB
+struct input_backend_data
 {
 	pepper_compositor_t *compositor;
 	struct wl_display *display;
 	struct wl_event_loop *event_loop;
-	struct wl_event_source *event_source;
-
-	pepper_input_device_t *device;
-
 	pepper_bool_t init;
+
+	pepper_list_t device_list;
 	pepper_list_t event_queue;
-} Input_Global_DB;
+};
 
 struct key_event
 {
 	int keycode;
 	int state;
+	pepper_input_device_t *device;
+
 	pepper_list_t link;
 };
 
-Input_Global_DB input_db;
+struct device_data
+{
+	pepper_input_device_t *device;
+	struct wl_event_source *event_source;
+
+	pepper_list_t link;
+};
+
+input_backend_data_t input_info;
 
 pepper_input_device_backend_t _headless_input_device_backend = {
 	_headless_input_device_get_property
@@ -82,7 +92,7 @@ _headless_input_get_current_time(void)
 }
 
 static void
-_headless_input_event_send(int keycode, int state)
+_headless_input_event_send(int keycode, int state, pepper_input_device_t *device)
 {
 	pepper_input_event_t event;
 
@@ -91,12 +101,12 @@ _headless_input_event_send(int keycode, int state)
 	if (state) event.state = PEPPER_KEY_STATE_PRESSED;
 	else event.state = PEPPER_KEY_STATE_RELEASED;
 
-	pepper_object_emit_event((pepper_object_t *)input_db.device,
-							 PEPPER_EVENT_INPUT_DEVICE_KEYBOARD_KEY, &event);
+	pepper_object_emit_event((pepper_object_t *)device,
+	                         PEPPER_EVENT_INPUT_DEVICE_KEYBOARD_KEY, &event);
 }
 
 static void
-_headless_input_event_queue(int keycode, int state)
+_headless_input_event_queue(int keycode, int state, pepper_input_device_t * device)
 {
 	key_event_t *kinfo;
 	kinfo = (key_event_t *)calloc(1, sizeof(key_event_t));
@@ -105,8 +115,9 @@ _headless_input_event_queue(int keycode, int state)
 	pepper_list_init(&kinfo->link);
 	kinfo->keycode = keycode;
 	kinfo->state = state;
+	kinfo->device = device;
 
-	pepper_list_insert(&input_db.event_queue, &kinfo->link);
+	pepper_list_insert(&input_info.event_queue, &kinfo->link);
 }
 
 static void
@@ -114,20 +125,19 @@ _headless_input_event_flush(void)
 {
 	key_event_t *kinfo, *ktmp;
 
-	pepper_list_for_each_safe(kinfo, ktmp, &input_db.event_queue, link) {
-		_headless_input_event_send(kinfo->keycode, kinfo->state);
+	pepper_list_for_each_safe(kinfo, ktmp, &input_info.event_queue, link) {
+		_headless_input_event_send(kinfo->keycode, kinfo->state, kinfo->device);
 		pepper_list_remove(&kinfo->link);
 		free(kinfo);
 	}
 }
 
 static void
-_headless_input_event_process(struct input_event *ev)
+_headless_input_event_process(struct input_event *ev, pepper_input_device_t *device)
 {
-	unsigned int time;
 	switch (ev->type) {
 		case EV_KEY:
-			_headless_input_event_queue(ev->code, ev->value);
+			_headless_input_event_queue(ev->code, ev->value, device);
 			break;
 		case EV_SYN:
 			_headless_input_event_flush();
@@ -146,7 +156,7 @@ _headless_input_backend_event_loop(int fd, uint32_t mask, void *data)
 	memset(iev, 0, sizeof(struct input_event)*SIZE_BUFFER);
 	if ((len = read(fd,  &iev, sizeof(iev))) != -1) {
 		for (i = 0; i < (int)(len / sizeof(iev[0])); i++) {
-			_headless_input_event_process(&iev[i]);
+			_headless_input_event_process(&iev[i], (pepper_input_device_t *)data);
 		}
 	}
 
@@ -157,15 +167,41 @@ static void
 _headless_input_open_device(const char *path)
 {
 	int fd;
+	device_data_t *device_info;
 
-	if ((fd = open(path, O_RDONLY)) < 0) {
+	if ((fd = open(path, O_RDONLY)) <= 0) {
 		PEPPER_ERROR("Failed to open %s\n", path);
 		return;
 	}
-	input_db.device = pepper_input_device_create(input_db.compositor, WL_SEAT_CAPABILITY_KEYBOARD,
-				   &_headless_input_device_backend, NULL);
-	input_db.event_source = wl_event_loop_add_fd(input_db.event_loop, fd, WL_EVENT_READABLE,
-	                                             _headless_input_backend_event_loop, NULL);
+
+	device_info = (device_data_t *)calloc(1, sizeof(device_data_t));
+	PEPPER_CHECK(device_info, return, "Failed to allocate memory for device\n");
+
+	pepper_list_init(&device_info->link);
+	device_info->device = pepper_input_device_create(input_info.compositor, WL_SEAT_CAPABILITY_KEYBOARD,
+	                                                 &_headless_input_device_backend, NULL);
+	PEPPER_CHECK(device_info->device, goto failed, "Failed to create pepper input device\n");
+
+	device_info->event_source = wl_event_loop_add_fd(input_info.event_loop, fd, WL_EVENT_READABLE,
+	                                                 _headless_input_backend_event_loop, device_info->device);
+	PEPPER_CHECK(device_info->event_source, goto failed, "Failed to create pepper input device\n");
+
+	pepper_list_insert(&input_info.device_list, &device_info->link);
+
+	return;
+
+failed:
+	if (device_info->device) {
+		pepper_input_device_destroy(device_info->device);
+		device_info->device = NULL;
+	}
+	if (device_info) {
+		free(device_info);
+		device_info = NULL;
+	}
+	if (fd > 0) {
+		close(fd);
+	}
 }
 
 PEPPER_API int
@@ -173,22 +209,24 @@ headless_input_backend_init(void *headless)
 {
 	PEPPER_TRACE("%s -- begin\n", __FUNCTION__);
 
-	input_db.compositor = (pepper_compositor_t *)headless;
-	PEPPER_CHECK(input_db.compositor, return 0, "Invalid compositor\n");
-	input_db.display = pepper_compositor_get_display(input_db.compositor);
-	PEPPER_CHECK(input_db.display, return 0, "Invalid wl_display\n");
-	input_db.event_loop = wl_display_get_event_loop(input_db.display);
-	PEPPER_CHECK(input_db.event_loop, return 0, "No event loop exist.\n");
+	input_info.compositor = (pepper_compositor_t *)headless;
+	PEPPER_CHECK(input_info.compositor, return 0, "Invalid compositor\n");
+	input_info.display = pepper_compositor_get_display(input_info.compositor);
+	PEPPER_CHECK(input_info.display, return 0, "Invalid wl_display\n");
+	input_info.event_loop = wl_display_get_event_loop(input_info.display);
+	PEPPER_CHECK(input_info.event_loop, return 0, "No event loop exist.\n");
 
-	if (input_db.init == PEPPER_TRUE) {
+	if (input_info.init == PEPPER_TRUE) {
 		PEPPER_ERROR("Current input backend is already initialized\n");
 		return 0;
 	}
 
-	pepper_list_init(&input_db.event_queue);
+	pepper_list_init(&input_info.event_queue);
+	pepper_list_init(&input_info.device_list);
 
-	input_db.init = PEPPER_TRUE;
+	input_info.init = PEPPER_TRUE;
 
+	_headless_input_open_device("/dev/input/event0");
 	_headless_input_open_device("/dev/input/event1");
 
 	PEPPER_TRACE("%s -- end\n", __FUNCTION__);
@@ -199,7 +237,23 @@ headless_input_backend_init(void *headless)
 PEPPER_API int
 headless_input_backend_fini(void *headless)
 {
+	device_data_t *dinfo, *dtmp;
+
 	PEPPER_TRACE("%s -- begin\n", __FUNCTION__);
+
+	/* Flush queued event */
+	if (!pepper_list_empty(&input_info.event_queue)) {
+		_headless_input_event_flush();
+	}
+	if (!pepper_list_empty(&input_info.device_list)) {
+		pepper_list_for_each_safe(dinfo, dtmp, &input_info.device_list, link) {
+			wl_event_source_remove(dinfo->event_source);
+			pepper_input_device_destroy(dinfo->device);
+			pepper_list_remove(&dinfo->link);
+			free(dinfo);
+		}
+	}
+
 	PEPPER_TRACE("%s -- end\n", __FUNCTION__);
 
 	return 0;
